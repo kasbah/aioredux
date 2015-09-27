674c84fb8aa1590992ded0f061708caca5c2e640
Allen Riddell <abr@ariddell.org>
2015-09-27 14:14:51 -0400

    TST: Add additional test

--- aioredux/tests/test_aioredux.py
+++ aioredux/tests/test_aioredux.py
@@ -1,3 +1,5 @@
+import toolz
+
 import aioredux
 from aioredux.tests import base
 
@@ -10,3 +12,27 @@ class TestAioredux(base.TestCase):
         }
         store = aioredux.Store(lambda state, action: state, initial_state)
         self.assertIsNotNone(store)
+        self.assertIsNotNone(store.state)
+
+    def test_todo_reducer(self):
+        initial_state = {
+            'todos': (),
+        }
+
+        def add_todo(text):
+            return {'type': 'ADD_TODO', 'text': text}
+
+        def reducer(state, action):
+            if action['type'] == 'ADD_TODO':
+                todos = state['todos'] + (action['text'],)
+                return toolz.assoc(state, 'todos', todos)
+            return state
+
+        store = aioredux.Store(reducer, initial_state)
+        self.assertIsNotNone(store.state)
+        self.assertIsNotNone(store.state['todos'])
+        self.assertEqual(len(store.state['todos']), 0)
+        store.dispatch(add_todo('todo text'))
+        self.assertIsNotNone(store.state)
+        self.assertIsNotNone(store.state['todos'])
+        self.assertEqual(len(store.state['todos']), 1)
