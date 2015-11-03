a2523230ff824246c2424706118e2078828d18ed
Allen Riddell <abr@ariddell.org>
2015-11-03 17:54:42 +0100

    Improve error messages

--- aioredux/middleware.py
+++ aioredux/middleware.py
@@ -4,9 +4,11 @@ import asyncio
 def thunk_middleware(dispatch, state_func):
     def next_func(next_handler):
         def action_func(action):
+            if asyncio.iscoroutine(action):
+                raise RuntimeError('Expected coroutine function, found coroutine: {}'.format(action))
             if callable(action):
                 if not asyncio.iscoroutinefunction(action):
-                    raise RuntimeError('thunk must be a coroutine function')
+                    raise RuntimeError('thunk must be a coroutine function, found callable: {}'.format(action))
                 return action(dispatch, state_func)
             else:
                 return next_handler(action)
