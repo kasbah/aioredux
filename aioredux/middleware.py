f8ded146e8ca6741c7259342c66d85ce66a63117
Allen Riddell <abr@ariddell.org>
2016-06-01 10:17:56 -0400

    Add simple logger middleware

--- aioredux/middleware.py
+++ aioredux/middleware.py
@@ -1,4 +1,9 @@
 import asyncio
+import json
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def thunk_middleware(dispatch, state_func):
@@ -10,5 +15,17 @@ def thunk_middleware(dispatch, state_func):
                 return action(dispatch, state_func)
             else:
                 return next_handler(action)
+        return action_func
+    return next_func
+
+
+def logger_middleware(dispatch, state_func):
+    def next_func(next_handler):
+        def action_func(action):
+            logger.info('PREV STATE: {}'.format(json.dumps(state_func())))
+            logger.info('ACTION: {}'.format(json.dumps(action)))
+            val = next_handler(action)
+            logger.info('NEW STATE: {}'.format(json.dumps(state_func())))
+            return val
         return action_func
     return next_func
